12ac0d3bf80aaaf6ec53af419151373c5b137c82
Rafael <rafael.tomaz@unesp.br>
2024-11-07 22:16:47 -0300

    DB funcionando

--- Readme.txt
+++ Readme.txt
@@ -6,6 +6,7 @@ node app.js
 // necessário adicionar em package.json a instrução na seção scripts:
 //"scripts": {
 //    "dev": "nodemon app.js"
+//    },
 //Permite que o servidor atualize automaticamente apos modificações salvas.
 npm run dev
 
--- routes/jobs.js
+++ routes/jobs.js
@@ -6,12 +6,15 @@ const express   = require('express');
 const router    = express.Router();
 const Job       = require('../models/Job');
 
+
+// Rota de teste
 router.get('/test', (req, res) => {
-    res.send("Working ");       // via postman, usa-se um get nessa rota pra testar a conexão
+    res.send("Funcionando - PI UNIVESP 2024\n Time de desenvolvimento. ");       // via postman, usa-se um get nessa rota pra testar a conexão
 });
 
 // add job via post
 router.post('/add' , (req, res) => {
+    
     let {tittle, salary, company, description, email , new_job }= req.body;
 
     // insert
